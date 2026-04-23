6d7ab32391c599a34ca69e0f783949041bfe0d83
Ido Engel <idoengel0707@gmail.com>
2026-04-23 19:41:28 +0300

    Init Commit

--- header/Utils/Utils.hpp
+++ header/Utils/Utils.hpp
@@ -1,13 +1,5 @@
 #pragma once
-#include <gtest/gtest.h>
 
-TEST(UtilsTest, utils1) {
-    EXPECT_EQ(1, 1);
-}
-
-TEST(UtilsTest, utils2) {
-    EXPECT_EQ(1, 1);
-}
 
 namespace Utils {
 
---
+++ src/Utils/Utils.cpp
@@ -0,0 +1,10 @@
+#include "../../header/Utils/Utils.hpp"
+#include <gtest/gtest.h>
+
+TEST(UtilsTest, utils1) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(UtilsTest, utils2) {
+    EXPECT_EQ(1, 1);
+}
