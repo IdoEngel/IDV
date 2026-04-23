7db398ef6b54e954deee956e06a8413c2be0b993
Ido Engel <idoengel0707@gmail.com>
2026-04-23 17:58:38 +0300

    test test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-TEST(SampleTest, SampleAssertion) {
+TEST(InitTest, Sample) {
     EXPECT_EQ(1, 1);
 }
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
+    
     std::cout << "Hello, IDV!!" << std::endl;
 
     return RUN_ALL_TESTS();
